faa074d32a212debb2847e6d4f1c6983160d1a5e
hbaughman <hbaughman741@gmail.com>
2016-07-17 05:38:20 -0400

    Extract single param

--- services/parser.js
+++ services/parser.js
@@ -7,6 +7,15 @@ function parser(str) {
     primary: remainder
   }
 
+  const reg = /(\w+)([=<>])(\w+|"[^"]*")/g
+  const match = reg.exec(remainder)
+  if (match) {
+    args[match[1]] = {
+      comparison: match[2],
+      value: match[3]
+    }
+  }
+
   return {
     command,
     args,
--- test/parser.spec.js
+++ test/parser.spec.js
@@ -3,7 +3,7 @@ const parser = require('../services/parser');
 
 describe("parser", function() {
   describe("return an object", function() {
-    it("should have a command", function() {
+    it("should have a command and args", function() {
       const str = "search chicken";
       const actual = parser(str).command;
       expect(actual).to.equal("search");
@@ -13,6 +13,17 @@ describe("parser", function() {
       const str = "search chicken";
       const actual = parser(str).args.primary;
       expect(actual).to.equal("chicken");
+    })
+
+
+    it("should extract a param (given a param)", function() {
+      const str = "search seller=Moes";
+      const actual = parser(str).args.seller;
+      const expected = {
+        comparison: "=",
+        value: "Moes"
+      }
+      expect(actual).to.deep.equal(expected)
     })
   })
 });
